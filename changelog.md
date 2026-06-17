4b46b1183caac6340b14d2aaa84e1e069de6d28c
rhncg <rohanchakgupta@gmail.com>
2026-06-16 21:29:38 -0700

    v1.1.0

--- changelog.md
+++ changelog.md
@@ -1,9 +1,13 @@
-# 1.0.0
-- Initial release
-
-# 1.0.1
-- GitHub Actions build republish
+# 1.1.0
+- Added option to enter positive gravity values (reverse gravity)
+- Added option to remove ceiling collisions
 
 # 1.0.2
 - Removed CCMenu hook
 - Modified collision hitbox size
+
+# 1.0.1
+- GitHub Actions build republish
+
+# 1.0.0
+- Initial release
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/CCMenu.hpp>
 #include <Geode/modify/CreatorLayer.hpp>
-#include <Geode/modify/CCMenu.hpp> 
 #include <box2d/box2d.h>
-#include <vector>
 #include <random>
+#include <vector>
 
 using namespace geode::prelude;
 
@@ -11,15 +11,13 @@ class ModCreatorLayer;
 
 constexpr float PTM_RATIO = 32.0f; // pixel to meter
 
-struct ButtonPhysics
-{
+struct ButtonPhysics {
     CCNode *node = nullptr;
     b2Body *body = nullptr;
 };
 
-class TouchPhysicsHandler : public CCLayer
-{
-public:
+class TouchPhysicsHandler : public CCLayer {
+  public:
     ModCreatorLayer *m_creatorLayer = nullptr;
 
     static TouchPhysicsHandler *create(ModCreatorLayer *layer);
@@ -27,12 +25,11 @@ public:
     virtual void ccTouchMoved(CCTouch *touch, CCEvent *event) override;
     virtual void ccTouchEnded(CCTouch *touch, CCEvent *event) override;
     virtual void registerWithTouchDispatcher() override;
+    virtual void onExit() override;
 };
 
-class $modify(ModCreatorLayer, CreatorLayer)
-{
-    struct Fields
-    {
+class $modify(ModCreatorLayer, CreatorLayer) {
+    struct Fields {
         std::vector<ButtonPhysics> m_physicsButtons;
         bool m_buttonsCaptured = false;
         bool m_physicsDraggingEnabled = false;
@@ -49,13 +46,11 @@ class $modify(ModCreatorLayer, CreatorLayer)
         }
     };
 
-    bool init()
-    {
+    bool init() {
         if (!CreatorLayer::init())
             return false;
 
-        if (!Mod::get()->getSettingValue<bool>("enable-physics"))
-        {
+        if (!Mod::get()->getSettingValue<bool>("enable-physics")) {
             return true;
         }
 
@@ -64,7 +59,8 @@ class $modify(ModCreatorLayer, CreatorLayer)
         m_fields->m_mouseJoint = nullptr;
         m_fields->m_physicsDraggingEnabled = false;
 
-        float gravityY = static_cast<float>(Mod::get()->getSettingValue<double>("physics-gravity"));
+        float gravityY = static_cast<float>(
+            Mod::get()->getSettingValue<double>("physics-gravity"));
 
         b2Vec2 gravity(0.0f, gravityY);
         m_fields->m_world = new b2World(gravity);
@@ -73,25 +69,23 @@ class $modify(ModCreatorLayer, CreatorLayer)
         b2BodyDef groundBodyDef;
         m_fields->m_groundBody = m_fields->m_world->CreateBody(&groundBodyDef);
 
-        auto menu = typeinfo_cast<CCMenu *>(this->getChildByID("creator-buttons-menu"));
-        if (menu)
-        {
-            menu->schedule(schedule_selector(ModCreatorLayer::customPhysicsUpdate));
+        auto menu =
+            typeinfo_cast<CCMenu *>(this->getChildByID("creator-buttons-menu"));
+        if (menu) {
+            menu->schedule(
+                schedule_selector(ModCreatorLayer::customPhysicsUpdate));
             menu->setTouchEnabled(true);
 
             auto touchOverlay = TouchPhysicsHandler::create(this);
-            if (touchOverlay)
-            {
+            if (touchOverlay) {
                 this->addChild(touchOverlay, 999);
                 touchOverlay->setTouchEnabled(true);
             }
 
             auto exitMenu = this->getChildByID("exit-menu");
-            if (exitMenu)
-            {
+            if (exitMenu) {
                 auto toggler = CCMenuItemToggler::createWithStandardSprites(
-                    this,
-                    menu_selector(ModCreatorLayer::onTogglePhysics),
+                    this, menu_selector(ModCreatorLayer::onTogglePhysics),
                     0.7f);
                 toggler->setID("physics-toggler"_spr);
                 toggler->toggle(m_fields->m_physicsDraggingEnabled);
@@ -102,8 +96,8 @@ class $modify(ModCreatorLayer, CreatorLayer)
                 label->setPosition({17, 0});
                 toggler->addChild(label);
 
-                if (auto layout = typeinfo_cast<AxisLayout *>(exitMenu->getLayout()))
-                {
+                if (auto layout =
+                        typeinfo_cast<AxisLayout *>(exitMenu->getLayout())) {
                     layout->setAxisReverse(true);
                     exitMenu->updateLayout();
                 }
@@ -112,19 +106,17 @@ class $modify(ModCreatorLayer, CreatorLayer)
         return true;
     }
 
-    void onTogglePhysics(CCObject *sender)
-    {
+    void onTogglePhysics(CCObject *sender) {
         auto toggler = typeinfo_cast<CCMenuItemToggler *>(sender);
         m_fields->m_physicsDraggingEnabled = !toggler->isToggled();
 
-        if (auto menu = typeinfo_cast<CCMenu*>(this->getChildByID("creator-buttons-menu")))
-        {
+        if (auto menu = typeinfo_cast<CCMenu *>(
+                this->getChildByID("creator-buttons-menu"))) {
             menu->setTouchEnabled(!m_fields->m_physicsDraggingEnabled);
         }
     }
 
-    void customPhysicsUpdate(float dt)
-    {
+    void customPhysicsUpdate(float dt) {
         auto menu = static_cast<CCMenu *>(static_cast<CCObject *>(this));
         if (!menu)
             return;
@@ -136,18 +128,19 @@ class $modify(ModCreatorLayer, CreatorLayer)
         if (dt <= 0.0f)
             dt = 1.0f / 60.0f;
 
-        if (!creatorLayer->m_fields->m_buttonsCaptured)
-        {
+        if (!creatorLayer->m_fields->m_buttonsCaptured) {
             menu->updateLayout();
             CCSize menuSize = menu->getContentSize();
             auto winSize = CCDirector::sharedDirector()->getWinSize();
 
             CCPoint botLeft = menu->convertToNodeSpace(CCPoint{0.0f, 0.0f});
-            CCPoint topRight = menu->convertToNodeSpace(CCPoint{winSize.width, winSize.height});
+            CCPoint topRight = menu->convertToNodeSpace(
+                CCPoint{winSize.width, winSize.height});
 
             b2BodyDef wallBodyDef;
             wallBodyDef.position.Set(0, 0);
-            b2Body *staticWalls = creatorLayer->m_fields->m_world->CreateBody(&wallBodyDef);
+            b2Body *staticWalls =
+                creatorLayer->m_fields->m_world->CreateBody(&wallBodyDef);
 
             b2EdgeShape wallShape;
             b2FixtureDef wallFixture;
@@ -155,21 +148,34 @@ class $modify(ModCreatorLayer, CreatorLayer)
             wallFixture.friction = 0.5f;
             wallFixture.restitution = 0.3f;
 
-            wallShape.SetTwoSided(b2Vec2(botLeft.x / PTM_RATIO, botLeft.y / PTM_RATIO), b2Vec2(topRight.x / PTM_RATIO, botLeft.y / PTM_RATIO));
+            float infiniteHeightY = topRight.y * 100.0f;
+
+            wallShape.SetTwoSided(
+                b2Vec2(botLeft.x / PTM_RATIO, botLeft.y / PTM_RATIO),
+                b2Vec2(topRight.x / PTM_RATIO, botLeft.y / PTM_RATIO));
             staticWalls->CreateFixture(&wallFixture);
 
-            wallShape.SetTwoSided(b2Vec2(botLeft.x / PTM_RATIO, topRight.y / PTM_RATIO), b2Vec2(topRight.x / PTM_RATIO, topRight.y / PTM_RATIO));
+            bool removeCeiling =
+                Mod::get()->getSettingValue<bool>("remove-ceiling");
+            if (!removeCeiling) {
+                wallShape.SetTwoSided(
+                    b2Vec2(botLeft.x / PTM_RATIO, topRight.y / PTM_RATIO),
+                    b2Vec2(topRight.x / PTM_RATIO, topRight.y / PTM_RATIO));
+                staticWalls->CreateFixture(&wallFixture);
+            }
+
+            wallShape.SetTwoSided(
+                b2Vec2(botLeft.x / PTM_RATIO, botLeft.y / PTM_RATIO),
+                b2Vec2(botLeft.x / PTM_RATIO, infiniteHeightY / PTM_RATIO));
             staticWalls->CreateFixture(&wallFixture);
 
-            wallShape.SetTwoSided(b2Vec2(botLeft.x / PTM_RATIO, botLeft.y / PTM_RATIO), b2Vec2(botLeft.x / PTM_RATIO, topRight.y / PTM_RATIO));
-            staticWalls->CreateFixture(&wallFixture);
-
-            wallShape.SetTwoSided(b2Vec2(topRight.x / PTM_RATIO, botLeft.y / PTM_RATIO), b2Vec2(topRight.x / PTM_RATIO, topRight.y / PTM_RATIO));
+            wallShape.SetTwoSided(
+                b2Vec2(topRight.x / PTM_RATIO, botLeft.y / PTM_RATIO),
+                b2Vec2(topRight.x / PTM_RATIO, infiniteHeightY / PTM_RATIO));
             staticWalls->CreateFixture(&wallFixture);
 
             auto children = menu->getChildren();
-            if (children && children->count() > 0)
-            {
+            if (children && children->count() > 0) {
                 std::random_device rd;
                 std::mt19937 gen(rd());
 
@@ -177,42 +183,50 @@ class $modify(ModCreatorLayer, CreatorLayer)
                 std::uniform_real_distribution<float> forceY(2.0f, 10.0f);
                 std::uniform_real_distribution<float> torque(-2.0f, 2.0f);
 
-                float customFriction = static_cast<float>(Mod::get()->getSettingValue<double>("physics-friction"));
-                float customBounciness = static_cast<float>(Mod::get()->getSettingValue<double>("physics-bounciness"));
+                float customFriction = static_cast<float>(
+                    Mod::get()->getSettingValue<double>("physics-friction"));
+                float customBounciness = static_cast<float>(
+                    Mod::get()->getSettingValue<double>("physics-bounciness"));
 
-                for (int i = 0; i < children->count(); ++i)
-                {
-                    auto btn = typeinfo_cast<CCNode *>(children->objectAtIndex(i));
-                    if (btn)
-                    {
+                for (int i = 0; i < children->count(); ++i) {
+                    auto btn =
+                        typeinfo_cast<CCNode *>(children->objectAtIndex(i));
+                    if (btn) {
                         CCPoint pos = btn->getPosition();
 
                         float hitboxScale = 0.887f;
-                        float w = btn->getScaledContentSize().width * hitboxScale;
-                        float h = btn->getScaledContentSize().height * hitboxScale;
+                        float w =
+                            btn->getScaledContentSize().width * hitboxScale;
+                        float h =
+                            btn->getScaledContentSize().height * hitboxScale;
 
                         b2BodyDef bodyDef;
                         bodyDef.type = b2_dynamicBody;
-                        bodyDef.position.Set(pos.x / PTM_RATIO, pos.y / PTM_RATIO);
+                        bodyDef.position.Set(pos.x / PTM_RATIO,
+                                             pos.y / PTM_RATIO);
                         bodyDef.angularDamping = 0.8f;
                         bodyDef.linearDamping = 0.1f;
 
-                        b2Body *body = creatorLayer->m_fields->m_world->CreateBody(&bodyDef);
+                        b2Body *body =
+                            creatorLayer->m_fields->m_world->CreateBody(
+                                &bodyDef);
 
                         b2PolygonShape boxShape;
-                        boxShape.SetAsBox((w * 0.5f) / PTM_RATIO, (h * 0.5f) / PTM_RATIO);
+                        boxShape.SetAsBox((w * 0.5f) / PTM_RATIO,
+                                          (h * 0.5f) / PTM_RATIO);
 
                         b2FixtureDef fixtureDef;
                         fixtureDef.shape = &boxShape;
                         fixtureDef.density = 1.0f;
-                        
+
                         fixtureDef.friction = customFriction;
                         fixtureDef.restitution = customBounciness;
-                        
+
                         body->CreateFixture(&fixtureDef);
 
                         float mass = body->GetMass();
-                        b2Vec2 randomImpulse(forceX(gen) * mass, forceY(gen) * mass);
+                        b2Vec2 randomImpulse(forceX(gen) * mass,
+                                             forceY(gen) * mass);
 
                         body->ApplyLinearImpulseToCenter(randomImpulse, true);
                         body->ApplyAngularImpulse(torque(gen) * mass, true);
@@ -232,25 +246,23 @@ class $modify(ModCreatorLayer, CreatorLayer)
 
         creatorLayer->m_fields->m_world->Step(dt, 8, 3);
 
-        for (auto &bp : creatorLayer->m_fields->m_physicsButtons)
-        {
+        for (auto &bp : creatorLayer->m_fields->m_physicsButtons) {
             if (!bp.node || !bp.body)
                 continue;
 
             b2Vec2 b2Pos = bp.body->GetPosition();
             float angle = bp.body->GetAngle();
 
-            bp.node->setPosition(CCPoint{b2Pos.x * PTM_RATIO, b2Pos.y * PTM_RATIO});
+            bp.node->setPosition(
+                CCPoint{b2Pos.x * PTM_RATIO, b2Pos.y * PTM_RATIO});
             bp.node->setRotation(-1.0f * CC_RADIANS_TO_DEGREES(angle));
         }
     }
 };
 
-TouchPhysicsHandler *TouchPhysicsHandler::create(ModCreatorLayer *layer)
-{
+TouchPhysicsHandler *TouchPhysicsHandler::create(ModCreatorLayer *layer) {
     auto ret = new TouchPhysicsHandler();
-    if (ret && ret->init())
-    {
+    if (ret && ret->init()) {
         ret->m_creatorLayer = layer;
         ret->autorelease();
         return ret;
@@ -259,30 +271,29 @@ TouchPhysicsHandler *TouchPhysicsHandler::create(ModCreatorLayer *layer)
     return nullptr;
 }
 
-bool TouchPhysicsHandler::ccTouchBegan(CCTouch *touch, CCEvent *event)
-{
-    if (!m_creatorLayer || !m_creatorLayer->m_fields->m_world || !m_creatorLayer->m_fields->m_buttonsCaptured)
+bool TouchPhysicsHandler::ccTouchBegan(CCTouch *touch, CCEvent *event) {
+    if (!m_creatorLayer || !m_creatorLayer->m_fields->m_world ||
+        !m_creatorLayer->m_fields->m_buttonsCaptured)
         return false;
 
     if (!m_creatorLayer->m_fields->m_physicsDraggingEnabled)
         return false;
 
-    auto menu = typeinfo_cast<CCMenu *>(m_creatorLayer->getChildByID("creator-buttons-menu"));
+    auto menu = typeinfo_cast<CCMenu *>(
+        m_creatorLayer->getChildByID("creator-buttons-menu"));
     if (!menu)
         return false;
 
     CCPoint localClickPos = menu->convertToNodeSpace(touch->getLocation());
-    b2Vec2 physicsTargetPos(localClickPos.x / PTM_RATIO, localClickPos.y / PTM_RATIO);
+    b2Vec2 physicsTargetPos(localClickPos.x / PTM_RATIO,
+                            localClickPos.y / PTM_RATIO);
 
-    for (auto &bp : m_creatorLayer->m_fields->m_physicsButtons)
-    {
+    for (auto &bp : m_creatorLayer->m_fields->m_physicsButtons) {
         if (!bp.body)
             continue;
 
-        for (b2Fixture *f = bp.body->GetFixtureList(); f; f = f->GetNext())
-        {
-            if (f->TestPoint(physicsTargetPos))
-            {
+        for (b2Fixture *f = bp.body->GetFixtureList(); f; f = f->GetNext()) {
+            if (f->TestPoint(physicsTargetPos)) {
                 b2MouseJointDef md;
                 md.bodyA = m_creatorLayer->m_fields->m_groundBody;
                 md.bodyB = bp.body;
@@ -291,9 +302,12 @@ bool TouchPhysicsHandler::ccTouchBegan(CCTouch *touch, CCEvent *event)
 
                 float frequencyHz = 5.0f;
                 float dampingRatio = 0.7f;
-                b2LinearStiffness(md.stiffness, md.damping, frequencyHz, dampingRatio, md.bodyA, md.bodyB);
+                b2LinearStiffness(md.stiffness, md.damping, frequencyHz,
+                                  dampingRatio, md.bodyA, md.bodyB);
 
-                m_creatorLayer->m_fields->m_mouseJoint = static_cast<b2MouseJoint *>(m_creatorLayer->m_fields->m_world->CreateJoint(&md));
+                m_creatorLayer->m_fields->m_mouseJoint =
+                    static_cast<b2MouseJoint *>(
+                        m_creatorLayer->m_fields->m_world->CreateJoint(&md));
                 bp.body->SetAwake(true);
                 return true;
             }
@@ -302,34 +316,39 @@ bool TouchPhysicsHandler::ccTouchBegan(CCTouch *touch, CCEvent *event)
     return false;
 }
 
-void TouchPhysicsHandler::ccTouchMoved(CCTouch *touch, CCEvent *event)
-{
-    bool isDragging = (m_creatorLayer && m_creatorLayer->m_fields->m_mouseJoint);
+void TouchPhysicsHandler::ccTouchMoved(CCTouch *touch, CCEvent *event) {
+    bool isDragging =
+        (m_creatorLayer && m_creatorLayer->m_fields->m_mouseJoint);
     if (!isDragging)
         return;
 
-    auto menu = typeinfo_cast<CCMenu *>(m_creatorLayer->getChildByID("creator-buttons-menu"));
-    if (menu)
-    {
+    auto menu = typeinfo_cast<CCMenu *>(
+        m_creatorLayer->getChildByID("creator-buttons-menu"));
+    if (menu) {
         CCPoint localClickPos = menu->convertToNodeSpace(touch->getLocation());
-        b2Vec2 physicsTargetPos(localClickPos.x / PTM_RATIO, localClickPos.y / PTM_RATIO);
+        b2Vec2 physicsTargetPos(localClickPos.x / PTM_RATIO,
+                                localClickPos.y / PTM_RATIO);
         m_creatorLayer->m_fields->m_mouseJoint->SetTarget(physicsTargetPos);
     }
 }
 
-void TouchPhysicsHandler::ccTouchEnded(CCTouch *touch, CCEvent *event)
-{
+void TouchPhysicsHandler::ccTouchEnded(CCTouch *touch, CCEvent *event) {
     if (!m_creatorLayer)
         return;
-    if (m_creatorLayer->m_fields->m_mouseJoint)
-    {
-        m_creatorLayer->m_fields->m_world->DestroyJoint(m_creatorLayer->m_fields->m_mouseJoint);
+    if (m_creatorLayer->m_fields->m_mouseJoint) {
+        m_creatorLayer->m_fields->m_world->DestroyJoint(
+            m_creatorLayer->m_fields->m_mouseJoint);
         m_creatorLayer->m_fields->m_mouseJoint = nullptr;
     }
 }
 
-void TouchPhysicsHandler::registerWithTouchDispatcher()
-{
+void TouchPhysicsHandler::registerWithTouchDispatcher() {
     auto dispatcher = CCDirector::sharedDirector()->getTouchDispatcher();
     dispatcher->addTargetedDelegate(this, -129, true);
+}
+
+void TouchPhysicsHandler::onExit() {
+    auto dispatcher = CCDirector::sharedDirector()->getTouchDispatcher();
+    dispatcher->removeDelegate(this);
+    CCLayer::onExit();
 }
